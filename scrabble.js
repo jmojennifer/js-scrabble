37f5f1947561827c1b58c177f514fb564f727252
Jennifer Owens <jmo.jennifer@gmail.com>
2016-11-16 11:32:03 -0800

    Scrabble.prototype.highestScoreFrom(arrayOfWords) & Scrabble.prototype.tieBreaker(word1, word2) working as intended

--- scrabble.js
+++ scrabble.js
@@ -1,5 +1,4 @@
-var Scrabble = function(word) {
-  this.word = word.toUpperCase();
+var Scrabble = function() {
 
   this.points = {
     "A": 1,
@@ -31,7 +30,8 @@ var Scrabble = function(word) {
   };
 };
 
-Scrabble.prototype.score = function() {
+Scrabble.prototype.score = function(word) {
+  this.word = word.toUpperCase();
   var sum = 0;
   var bonus = 0;
   for (var key in this.points) {
@@ -47,8 +47,63 @@ Scrabble.prototype.score = function() {
   return sum;
 };
 
-my_game = new Scrabble("hello");
-console.log(my_game.score());
+Scrabble.prototype.highestScoreFrom = function(arrayOfWords) {
 
+  var highestScore = ["", 0];
+
+  for (var i = 0; i < arrayOfWords.length; i++) {
+    var word = arrayOfWords[i];
+
+    if (this.score(word) > highestScore[1]) {
+      highestScore = [word, this.score(word)];
+    }
+    else if (this.score(word) == highestScore[1]) {
+      highestScore = this.tieBreaker(highestScore, [word, this.score(word)]);
+    }
+  }
+  return highestScore[0];
+};
+
+Scrabble.prototype.tieBreaker = function(word1, word2) {
+  if (word1[0].length > word2[0].length) {
+    if (word1[0].length != 7) {
+      return word2;
+    }
+    else {
+      return word1;
+    }
+  }
+  else if (word1[0].length < word2[0].length) {
+    if (word2[0].length != 7) {
+      return word1;
+    }
+    else {
+      return word2;
+    }
+  }
+  else if (word1[0].length == word2[0].length) {
+    return word1;
+  }
+};
+
+my_game = new Scrabble();
+
+var triedWords = ["ZQQQQJ", "QQQQQJ", "DOG"];
+console.log(my_game.highestScoreFrom(triedWords));
+
+console.log("========");
+
+var triedWords = ["QQQQQJ", "GORILLA", "DOG"];
+console.log(my_game.highestScoreFrom(triedWords));
+
+console.log("========");
+
+var triedWords = ["Hello", "world", "work", "please"];
+console.log(my_game.highestScoreFrom(triedWords));
+
+console.log("========");
+
+var triedWords = ["QQQQJ", "QQQQQJ", "ZZQQQJ", "DOG"];
+console.log(my_game.highestScoreFrom(triedWords));
 
 module.exports = Scrabble;
